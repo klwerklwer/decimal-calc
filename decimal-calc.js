8f0d664cf3292ba9230a70823767eacdc1ea63ad
klwerklwer <69866421+klwerklwer@users.noreply.github.com>
2024-12-15 15:05:44 +0800

    Update decimal-calc.js

--- decimal-calc.js
+++ decimal-calc.js
@@ -1,5 +1,5 @@
 //用于高精度浮点型运算扩展
-(function(){
+(()=>{
 	function exponential( a ){
 		if( Number.isInteger( a ) ){
 			return [ a , 0 ]
@@ -17,28 +17,29 @@
 			return [ +(a[0] + a[1]) , a[1].length ]
 		}
 	}
-	Number.prototype.toFiniteNumber = function(){
+	Number.prototype.toFinite = function(){
 		return isFinite( this ) ? this.valueOf() : 0
 	}
+	Number.prototype.toFiniteNumber = Number.prototype.toFinite
 	Number.prototype['*'] = function( b ){
-		b = ( +b ).toFiniteNumber()
-		const a = this.toFiniteNumber()
+		b = ( +b ).toFinite()
+		const a = this.toFinite()
 		if( Number.isInteger( a ) && Number.isInteger( b ) ) return a * b
 		const [ a_int , a_p ] = exponential( a ),
 		[ b_int , b_p ] = exponential( b )
 		return a_int * b_int / 10**( a_p + b_p )
 	}
 	Number.prototype["/"] = function( b ) {
-		b = ( +b ).toFiniteNumber()
-		const a = this.toFiniteNumber()
-		if( Number.isInteger( a ) && Number.isInteger( b ) ) return (a / b).toFiniteNumber()
+		b = ( +b ).toFinite()
+		const a = this.toFinite()
+		if( Number.isInteger( a ) && Number.isInteger( b ) ) return (a / b).toFinite()
 		const [ a_int , a_p ] = exponential( a ),
 		[ b_int , b_p ] = exponential( b )
-		return (+(a_int / b_int + "e" + ( b_p - a_p ))).toFiniteNumber()
+		return (+(a_int / b_int + "e" + ( b_p - a_p ))).toFinite()
 	}
 	Number.prototype['+'] = function( b ) {
-		b = ( +b ).toFiniteNumber()
-		const a = this.toFiniteNumber()
+		b = ( +b ).toFinite()
+		const a = this.toFinite()
 		if( Number.isInteger( a ) && Number.isInteger( b ) ) return a + b
 		let [ a_int , a_p ] = exponential( a ),
 		[ b_int , b_p ] = exponential( b ),
@@ -54,8 +55,8 @@
 		return ( a_int + b_int ) / 10**c
 	}
 	Number.prototype['-'] = function( b ) {
-		b = ( +b ).toFiniteNumber()
-		const a = this.toFiniteNumber()
+		b = ( +b ).toFinite()
+		const a = this.toFinite()
 		if( Number.isInteger( a ) && Number.isInteger( b ) ) return a - b
 		let [ a_int , a_p ] = exponential( a ),
 		[ b_int , b_p ] = exponential( b ),
@@ -70,32 +71,28 @@
 		}
 		return ( a_int - b_int ) / 10**c
 	}
-	for( let k of [ "round" , "ceil" , "floor" ] ){
+	for( let k of [ "round" , "ceil" , "floor" , "trunc"] ){
 		Number.prototype[ k ] = function( s ){
-			let a = this.toFiniteNumber()
-			if( !a ){
+			let a = this.toFinite()
+			if( Number.isInteger(a) ){
 				return a
 			}
 			let [ int_part , float_part ] = a.toExponential().split("e")
-			s = (+s).toFiniteNumber()
+			s = (+s).toFinite()
 			float_part = +float_part + s
 			a = Math[ k ]( int_part + "e" + float_part )
 			let [ int_part2 , float_part2 ] = a.toExponential().split("e")
 			return +( int_part2 + "e" + ( +float_part2 - s ))
 		}
 	}
-	Number.prototype.roundDown = function( s ){
-		let a = this.toFiniteNumber()
-		if( !a ){
-			return a
-		}
-		let [ int_part , float_part ] = a.toExponential().split("e")
-		s = (+s).toFiniteNumber()
-		a = Math.trunc( int_part + "e" + (+float_part + s ) )
-		let [ int_part2 , float_part2 ] = a.toExponential().split("e")
-		return +( int_part2 + "e" + ( +float_part2 - s ))
+	Number.prototype.roundDown = Number.prototype.trunc
+	Number.prototype.toRatio = function( ratio , precision = 0 ){
+		return this["*"]( 10**ratio ).trunc( precision )
 	}
-	for( let k of [ 'toFiniteNumber' , '*' , '/' , '+' , '-' , 'round' , 'ceil' , 'floor' , 'roundDown' ]){
+	Number.prototype.clamp = function( min = 0 , max = Number.MAX_VALUE ){
+		return Math.min( Math.max( this , min ) , max ).toFinite()
+	}
+	for( let k of [ "toFinite" , 'toFiniteNumber' , '*' , '/' , '+' , '-' , 'round' , 'ceil' , 'floor' , 'roundDown' ]){
 		String.prototype[ k ] = function(){
 			return ( +this )[ k ]( ...arguments )
 		}
